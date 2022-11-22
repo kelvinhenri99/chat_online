53677b41bc8049ff24f3589ad0b3b7335601788b
kelvinhenri99 <kelvin5henri@gmail.com>
2022-11-22 07:33:44 -0300

    Implementando o welcome

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ChatController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ChatController::class, 'welcome']);
 
 Route::middleware([
     'auth:sanctum',
